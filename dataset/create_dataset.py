9997385215eaa37f420f9af4bde3444a4809b2e6
GasiorMateusz <mateuszgasior314@gmail.com>
2025-05-11 18:55:45 +0200

    add exemplatory usage of river_data_collector

--- dataset/create_dataset.py
+++ dataset/create_dataset.py
@@ -1,39 +1,23 @@
+from datetime import date
 import os
-import io
-import requests
-from bs4 import BeautifulSoup
-import zipfile
+from river_data_collector.river_downloader import imgw
+import json
 
-# Base URL of the data source
-base_url = 'https://danepubliczne.imgw.pl/pl/datastore/getfiledown/Arch/Telemetria/Hydro/' #2023/Hydro_2023-02.zip
-# Output folder to save downloaded files
-output_folder = "data"
+output_folder = "data/"
 os.makedirs(output_folder, exist_ok=True)
 
-years_to_download = [2025]
+downloader = imgw.ImgwDownloader()
+river_name = imgw.PolishRiversNames.BIALKA.value
+station_name = imgw.StationsNames.TRYBSZ2.value
+since_date = date(2023, 1, 1)
+till_date = date(2023, 1, 1)
 
-def download_and_extract_file(url, output_path):
-    """Download a file from a given URL and save it to the specified path."""
-    response = requests.get(base_url)
-    if response.status_code == 200:
-        z = zipfile.ZipFile(io.BytesIO(response.content))
-        z.extractall(output_path)
-        print(f"Downloaded and extracted: {output_path}")
-    else:
-        print(f"Failed to download: {url} (Status: {response.status_code})")
+data = downloader.get_river_data(river_name, station_name, since_date, till_date, False)
+if data:
+    for measurement_name, collection in data.items():
+        with open(f"{output_folder}{measurement_name}.json", "w") as file:
+            json.dump([measurement.__dict__ for measurement in collection.measurements], file, default=str)
+    print(f"Data saved to {measurement_name}.json")
+else:
+    print("No data collected.")
 
-def unpack_zip(file_path, extract_to):
-            """Unpack a zip file to the specified directory."""
-            try:
-                with zipfile.ZipFile(file_path, 'r') as zip_ref:
-                    zip_ref.extractall(extract_to)
-                print(f"Unpacked: {file_path}")
-            except zipfile.BadZipFile:
-                print(f"Failed to unpack: {file_path} (Bad zip file)")
-
-for year in years_to_download:
-    for month in range(1, 5):
-        file_name = f"Hydro_{year}-{month:02d}.zip"
-        file_path = os.path.join(output_folder, file_name) 
-        url = f"{base_url}{year}/{file_name}"
-        download_and_extract_file(url, file_path)
